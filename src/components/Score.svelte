8ff531bc9eb2ef9ccd8b6d14f365b016958953f7
bencohendev <ben@bencohen.dev>
2022-08-16 16:51:55 -0400

    card stamped with user icon

--- src/components/Score.svelte
+++ src/components/Score.svelte
@@ -1,22 +1,29 @@
 <script>
 	export let score = [];
 	export let currentCard;
+
+	import UserIcon from '$c/UserIcon.svelte';
 </script>
 
-<div class="flex h-32 mt-2">
+<div class="flex h-40 mt-2">
 	{#each score as card, i}
 		{@const { rowOne, rowTwo, rowThree, name } = card}
 		<div
-			class="p-2 mx-2 border border-gray-300 {i === currentCard
-				? 'bg-green-500'
-				: ''} "
+			class="p-2 mx-2 border border-gray-300 
+			{i === currentCard ? 'bg-green-500' : ''} "
 		>
-			<div class="h-1/3 flex items-center">{rowThree ? rowThree : ''}</div>
+			<div class="h-1/4 flex justify-center items-center">
+				{rowThree ? rowThree : ''}
+			</div>
 			<hr />
-			<div class="h-1/3 flex items-center">{rowTwo ? rowTwo : ''}</div>
+			<div class="h-1/4 flex justify-center items-center">
+				{rowTwo ? rowTwo : ''}
+			</div>
 			<hr />
-			<div class="h-1/3 flex items-center">{rowOne}</div>
-			<div>{name || ''}</div>
+			<div class="h-1/4 flex justify-center items-center">{rowOne}</div>
+			<hr />
+
+			<div class="h-1/4 mt-2"><UserIcon {name} /></div>
 		</div>
 	{:else}
 		<div>Please add a card to get started</div>
---
+++ src/components/UserIcon.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let name = '';
+	console.log('🚀 ~ file: UserIcon.svelte ~ line 3 ~ name', name);
+
+	const firstLetter = name.charAt(0).toUpperCase();
+</script>
+
+{#if firstLetter}
+	<div
+		class="w-6 h-6 
+      flex justify-center items-center
+      border-2 border-blue-500 rounded-full
+  "
+	>
+		<div class="relative">
+			{firstLetter}
+		</div>
+	</div>
+{/if}
